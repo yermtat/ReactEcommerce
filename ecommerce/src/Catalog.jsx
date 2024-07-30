9c10976e1a9cc32ea32f56c8ddb53e2e0814387d
yermtat <yerm.tat@gmail.com>
2024-07-30 23:38:36 +0400

    categories checkboxes

--- ecommerce/src/Catalog.jsx
+++ ecommerce/src/Catalog.jsx
@@ -1,10 +1,71 @@
-import React from "react";
-import { products } from "./Data";
+import React, { useRef, useEffect, useState } from "react";
+import { products, categories, colors } from "./Data";
 
 export default function Catalog() {
+  const category = useRef("all");
+  const [showpPoducts, setShowProducts] = useState(products);
+
   return (
     <div>
-      <div className="h-3/4 flex justify-center items-center relative p-20 mb-20">
+      <div className="h-3/4 flex justify-center relative p-20 mb-20">
+        <div className="px-4 border-r">
+          <div>
+            <h1 className="font-bold text-xl m-5">Categories</h1>
+            <ul>
+              <div class="flex items-center mb-4">
+                <input
+                  id="default-checkbox"
+                  type="checkbox"
+                  value="all"
+                  class="w-4 h-4 text-orange-400 bg-gray-100 border-gray-300 rounded focus:ring-orange-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  for="default-checkbox"
+                  class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  All
+                </label>
+              </div>
+              {categories.map((x) => (
+                <div class="flex items-center mb-4">
+                  <input
+                    id="default-checkbox"
+                    type="checkbox"
+                    value={x}
+                    class="w-4 h-4 text-orange-400 bg-gray-100 border-gray-300 rounded focus:ring-orange-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label
+                    for="default-checkbox"
+                    class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    {x}
+                  </label>
+                </div>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h1 className="font-bold text-xl m-5">Colors</h1>
+            <ul>
+              {colors.map((x) => (
+                <div class="flex items-center mb-4">
+                  <input
+                    id="default-checkbox"
+                    type="checkbox"
+                    value=""
+                    class="w-4 h-4 text-orange-400 bg-gray-100 border-gray-300 rounded focus:ring-orange-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label
+                    for="default-checkbox"
+                    class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    {x}
+                  </label>
+                </div>
+              ))}
+            </ul>
+          </div>
+        </div>
         <ul className="flex flex-wrap">
           {products.map((x) => (
             <li className="mx-10">
